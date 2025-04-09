0fbd128b5d18483f80f1bbf574c1f12936b7b553
LuixzSouza <luiz.antoniodesouza004@gmail.com>
2025-04-09 14:47:27 -0300

    Inserindo Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Se está usando src/
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        DarkBlue: '#002157',
+        orange: '#F47127',
+        gray: '#EBEDEF'
+      }
+    },
+  },
+  plugins: [],
+};
